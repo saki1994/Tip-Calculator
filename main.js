7e002c02e768ac20e4535e94ac29e1f5e0a15c15
saki1994 <67071604+saki1994@users.noreply.github.com>
2021-12-09 21:07:48 +0100

    adjusting grid size

--- main.js
+++ main.js
@@ -1,40 +1,40 @@
+let ele = document.querySelectorAll(".btn");
 
-let ele = document.querySelectorAll(".btn"); 
- 
 function calculate() {
-    let bill = parseFloat(document.querySelector("#bill").value);
-        let people = parseFloat(document.querySelector("#people").value); 
-        let custom = parseInt(document.querySelector("#btn-6").value);
-        let tipPerPerson =   document.querySelector("#tip-per-person");
-        let totalPerPerson =   document.querySelector("#total-per-person");
-        if(custom) {
-            clearRadioBtn();
-            let totalTip = Number.parseFloat(((custom / 100) * bill));
-            tipPerPerson.textContent = Number.parseFloat(totalTip / people).toFixed(2); 
-            totalPerPerson.textContent = Number.parseFloat((totalTip + bill) / people).toFixed(2);  
-        }  else{ 
-        let tip = document.querySelector('input[name = "tip"]:checked').value; 
-        tipPerPerson.textContent = Number.parseFloat(((tip * bill) - bill) / people).toFixed(2); 
-        totalPerPerson.textContent = Number.parseFloat((tip * bill) / people).toFixed(2)
-        }
+  let bill = parseFloat(document.querySelector("#bill").value);
+  let people = parseFloat(document.querySelector("#people").value);
+  let custom = parseInt(document.querySelector("#btn-6").value);
+  let tipPerPerson = document.querySelector("#tip-per-person");
+  let totalPerPerson = document.querySelector("#total-per-person");
+  if (custom) {
+    clearRadioBtn();
+    let totalTip = Number.parseFloat(custom / 100 * bill);
+    tipPerPerson.textContent = Number.parseFloat(totalTip / people).toFixed(2);
+    totalPerPerson.textContent = Number.parseFloat(
+      (totalTip + bill) / people
+    ).toFixed(2);
+  } else {
+    let tip = document.querySelector('input[name = "tip"]:checked').value;
+    tipPerPerson.textContent = Number.parseFloat(
+      (tip * bill - bill) / people
+    ).toFixed(2);
+    totalPerPerson.textContent = Number.parseFloat(tip * bill / people).toFixed(
+      2
+    );
+  }
 }
 
- 
-function reset() { 
-    document.querySelector("#bill").value = ""; 
-    document.querySelector("#people").value = "";
-    document.querySelector("#btn-6").value = "";
-    document.querySelector("#tip-per-person").textContent = '$0.00';
-    document.querySelector("#total-per-person").textContent = '$0.00';
-    clearRadioBtn();
+function reset() {
+  document.querySelector("#bill").value = "";
+  document.querySelector("#people").value = "";
+  document.querySelector("#btn-6").value = "";
+  document.querySelector("#tip-per-person").textContent = "$0.00";
+  document.querySelector("#total-per-person").textContent = "$0.00";
+  clearRadioBtn();
 }
 
 const clearRadioBtn = () => {
-    for(var i=0;i<ele.length;i++){
-       ele[i].checked = false; 
-    }
+  for (var i = 0; i < ele.length; i++) {
+    ele[i].checked = false;
+  }
 };
-  
-
-
- 
